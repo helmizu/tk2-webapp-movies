146a922f52f5d201ee96cbd301097dada12c929d
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2022-12-19 00:24:33 +0700

    update score range

--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -11,7 +11,7 @@ interface iFilmProps {
 
 const Film = ({ image = "", title = "", score = 0, year = 0 }: iFilmProps) => {
   const slug = convertToSlug(title);
-  const emote = score > 50 ? <FaceSmileIcon className="w-5 h-5 inline-block text-green-400" /> : <FaceFrownIcon className="w-5 h-5 inline-block text-red-400" />;
+  const emote = score > 5 ? <FaceSmileIcon className="w-5 h-5 inline-block text-green-400" /> : <FaceFrownIcon className="w-5 h-5 inline-block text-red-400" />;
   return (
     <Link to={`/detail/${slug}`}>
       <div className="card border-0 bg-[#1F1F1F] h-[100%]">
@@ -19,7 +19,7 @@ const Film = ({ image = "", title = "", score = 0, year = 0 }: iFilmProps) => {
           <img src={image} alt="banner" className="card-img-top img-fluid max-h-72 h-[100%] aspect-[3/4]" />
         </div>
         <div className="card-body">
-          <div className="text-end mb-2">{emote} {score}/<small className="align-top">100</small></div>
+          <div className="text-end mb-2">{emote} {score}/<small className="align-top">10</small></div>
           <p>
             <span className="fw-semibold">{title}</span>
             <br />
